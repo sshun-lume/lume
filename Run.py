cf3fd1c51bf58f13e84e3e5b1e80e38c9531cf6b
SShun <264180455+sshun-lume@users.noreply.github.com>
2026-03-04 21:59:52 +0900

    キューイングモードでも serialize 対応

--- Run.py
+++ Run.py
@@ -7,6 +7,7 @@ RUN_SCRIPT = os.getenv("RUN_SCRIPT")
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
 RUN_NAME = os.getenv("RUN_NAME")
 ARCHIVE_COMMAND = os.getenv("ARCHIVE_COMMAND")
+PREV_RUNID = os.getenv("PREV_RUNID")
 mlflow_run_id = sys.argv[1]
 
 sim_params = json.loads(os.getenv("SIM_PARAMS"))
@@ -30,7 +31,18 @@ try:
     ##
     sim.log_GPU_info(mlflow.log_params)
     mlflow.log_params(sim_params)
-    sim.create_initial_state(sim_params, mlflow.log_params, dump_files[1])
+    
+    if PREV_RUNID:
+        prev_state_path = mlflow.artifacts.download_artifacts(
+            run_id=PREV_RUNID,
+            artifact_path="final_state",    # 取得したいアーティファクト内のパス
+        )
+        files = os.listdir(prev_state_path)
+        sim.load_previous_state(f'{prev_state_path}/{files[0]}')
+        os.system(f"rm -rf {prev_state_path}")
+        mlflow.log_param("prev_run_id", PREV_RUNID)
+    else:
+        sim.create_initial_state(sim_params, mlflow.log_params, dump_files[1])
 
     ##
     ## シミュレーション処理　メインループ
@@ -49,7 +61,7 @@ finally:
             os.system(f"{ARCHIVE_COMMAND} {dump_file}")
             mlflow.log_artifact(f"{dump_file}.xz", artifact_path='dump')
 
-    mlflow.log_artifact(f'{RUN_NAME}.out', artifact_path='output')
-    mlflow.log_artifact(f'{RUN_NAME}.err', artifact_path='output')
-    
-    mlflow.end_run()
+mlflow.log_artifact(f'{RUN_NAME}.out', artifact_path='output')
+mlflow.log_artifact(f'{RUN_NAME}.err', artifact_path='output')
+
+mlflow.end_run()
